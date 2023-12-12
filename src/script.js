5f0bea47272c2f92e81ee6a48c82502f3a313edf
Ameer <ameerapex496@gmail.com>
2023-12-12 14:21:18 +0100

    added comment + added eventlisnters and selected elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-console.log("hallo world");
+// selecting our elements
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("openBtn");
+// global veribales
+// functions
+//event lisnters
+openBtn.addEventListener("click", function () {
+    console.log("hi ");
+});
+closeBtn.addEventListener("click", function () {
+    console.log("hi ");
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,16 @@
-console.log("hallo world");
+// selecting our elements
+const openBtn: any = document.getElementById("openBtn");
+const closeBtn: any = document.getElementById("openBtn");
+
+// global veribales
+
+// functions
+
+//event lisnters
+openBtn.addEventListener("click", () => {
+  console.log("hi ");
+});
+
+closeBtn.addEventListener("click", () => {
+  console.log("hi ");
+});
